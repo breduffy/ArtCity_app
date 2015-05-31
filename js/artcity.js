ce1fb689219af8c645e2d40abb166a5ae91fc306
breduffy <breanne.duffy@gmail.com>
2015-05-31 18:50:33 -0400

    Created initial file structure. Created mockups of the project in Balsamiq. Started HTML5 and CSS.

---
+++ js/artcity.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// var gameRef;
+// var gameRef, gameAuth, player;
+
+//Make sure the document is ready before applying jQuery library
+$(document).ready(function(){
+
+//Attempt to make modal hide
+  // $('#uploadArt').modal({
+  //   show:false;
+  // });
+
+
+});//End of document ready
