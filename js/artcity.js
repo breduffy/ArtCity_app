e2a99361e33ad14d1b0877daef49ba8887d8a5b6
breduffy <breanne.duffy@gmail.com>
2015-06-04 12:01:13 -0400

    Filter by city is working. Image validations commented out.

--- js/artcity.js
+++ js/artcity.js
@@ -27,13 +27,7 @@ $(document).ready(function(){
     $('html, body').animate({scrollTop: $("#filters").offset().top}, 2000);
   });
 
-  //FIXME: Try to get value of dropdown selector for cities
-  // Still needs to store the value
-  // var chooseCity;
-  $('a').click(function(){
-      var chooseCity = $(this).text();
-      alert(chooseCity);
-    });
+
 
 
   //FIXME: Attempt to make filtering by attribute
@@ -84,28 +78,72 @@ $(document).ready(function(){
 
   // });
 
+// // TO CREATE ARTWORK
+// var file = $('#upload-art-from-computer')[0].files[0];
+// // <input id="#yourfancyuploadimagefileinput" type='file'/>
+
+// var fd = new FormData();
+// fd.append('image', file);
+// fd.append('title', YOURJQUERY.val here);
+// fd.append('artist', YOURJQUERY.val here);
+// fd.append('venue', YOURJQUERY.val here);
+// fd.append('neighborhood', YOURJQUERY.val here);
+// fd.append('city', YOURJQUERY.val here);
+// fd.append('description', YOURJQUERY.val here);
+// // gather all of your other form data
+
+// $.ajax({
+//   url: YOUR BACKEND ur,
+//   data: fd,
+//   type: 'POST',
+//   contentType: false,
+//   cache: false,
+//   processData: false
+// }).done(function(response){
+
+//   // whatever you want to happen afterward here
+// });
+
+//append all of your columns to the fd using the above syntax!
+
 
   //HANDLEBARS TEMPLATE CONTROLLER
 
     var artworks = (function(){
+
+      var chosenCity = 'boston';
+      var defaultCity = 'boston';
+
+      var possibleCities = {
+        "Boston": 'boston',
+        "New York": 'newyork',
+        "Chicago": 'chicago',
+        "Los Angeles": 'losangeles'
+      };
+
+      var _recordChosenCity = function(choice){
+        chosenCity = possibleCities[choice] || defaultCity;
+      };
 
     //reaching out to a database and grabbing the artwork info and then pushing into html template
     //taking Json(a way of storing data) and pulling it into an array
 
     //attempt to make it searchable by city by changing boston to #{}?
     var getArtworks = function(){
-      $.get( "http://localhost:3000/venues/artwork?city=boston").done(function(response){
+      $.get( "http://localhost:3000/venues/artwork?city=" + chosenCity).done(function(response){
         _renderArtworks(response);
       });
     };
 
+    var templatingFunction;
 
   //printing the data
     var _renderArtworks = function(artworks){
 
       // your code starts here
       //extracting the html and putting into handlebars. Handlebars is a bower component, you installed handlebars and bootstrap
-      var templatingFunction = Handlebars.compile($('#artwork-index').html());
+      templatingFunction = templatingFunction || Handlebars.compile($('#artwork-index').html());
+      console.log($('#artwork-index').html());
 
       //the first characters is a key, the second is the value
       var result = templatingFunction({
@@ -118,10 +156,23 @@ $(document).ready(function(){
     };
 
     return {
-      indexArtworks: getArtworks
+      indexArtworks: getArtworks,
+      recordChosenCity: _recordChosenCity
     };
 
     })();
+
+  //FIXME: Try to get value of dropdown selector for cities
+  // Still needs to store the value
+  // var chooseCity;
+
+
+  $('a').click(function(){
+      var chosenCity = $(this).text();
+      artworks.recordChosenCity(chosenCity);
+      alert(chosenCity);
+    });
+
 
     //function calls your artwork handlebars template
     $("#filter-city-button").click(function(){
@@ -135,6 +186,29 @@ $(document).ready(function(){
 
   // Create a POST AJAX (upload art) submission and check if filled out correctly
 
+
+
+  //FIXME: Button trigger for image file upload
+  // $(document).on('change', '.btn-file :file', function() {
+  //   var input = $(this),
+  //   numFiles = input.get(0).files ? input.get(0).files.length : 1,
+  //   label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  //   input.trigger('fileselect', [numFiles, label]);
+  // });
+
+  // $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+
+  //     var input = $(this).parents('.input-group').find(':text'),
+  //     log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+  //     if( input.length ) {
+  //       input.val(log);
+  //     } else {
+  //       if( log ) alert(log);
+  //     }
+
+
+  // });
 
 
 });//End of document ready
